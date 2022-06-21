498cc52fa0246aa81fe7a0189776c6d77dc6ff06
Swati A Firangi <swatifirangi29@gmail.com>
2022-06-21 23:27:38 +0530

    add pseudocode for the game

---
+++ ladder-game.js
@@ -0,0 +1,24 @@
+/*
+if(level is 1) --> player starts at bottom
+
+coin is tossed at level 1:
+
+if heads --> moves to level 2
+if tails --> stays on level 1
+coin is tossed at level 2:
+
+if heads --> moves to level 3
+if tails --> moves on level 1
+coin is tossed at level 3:
+
+if heads --> moves to level 4
+if tails --> moves to level 1
+coin is tossed at level 4:
+
+if heads --> moves to level 5
+if tails --> moves to level 1
+coin is tossed at level 5:
+
+if heads --> won the game
+if tails --> moves to level 1
+*/
